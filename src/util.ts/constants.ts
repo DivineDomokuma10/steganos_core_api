a5e5ac6aa7fd9d50c4dc498b314243b62a577cf1
DivineDomokuma10 <divinedomokuma@gmail.com>
2026-05-24 22:53:21 +0100

    chore: add client url to CORS config

--- src/util.ts/constants.ts
+++ src/util.ts/constants.ts
@@ -4,6 +4,8 @@ export const DEV_ALLOWED_ORIGINS = [
   "http://localhost:3000",
   "http://192.168.143.150:3000",
 ];
-export const PROD_ALLOWED_ORIGINS = ["https://steganoscore.onrender.com"];
+export const PROD_ALLOWED_ORIGINS = [
+  "https://steganos-core-client.onrender.com",
+];
 
 export const REFRESH_TOKEN_TTL = 5 * 60 * 60 * 1000;
