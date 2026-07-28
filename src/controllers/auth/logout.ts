857bdc7eec3ddc91a9a79bf95693ee2fd7a222a4
DivineDomokuma10 <divinedomokuma@gmail.com>
2026-07-29 00:07:45 +0100

    fix: logout logic

--- src/controllers/auth/logout.ts
+++ src/controllers/auth/logout.ts
@@ -1,9 +1,27 @@
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 import { apiResponse } from "@/util";
+import { config } from "@/config";
 import { clearCookie } from "@/services/auth/cookie";
+import { IRefreshJwtPayload } from "@/types/interface";
+import RefreshTokenModel from "@/model/refresh-token.model";
 
-const logoutController = async (_req: Request, res: Response) => {
+const logoutController = async (req: Request, res: Response) => {
+  const token = req.cookies?.refreshToken;
+
+  if (token) {
+    const decoded = jwt.verify(
+      token,
+      config.refreshTokenSecret,
+    ) as IRefreshJwtPayload;
+
+    await RefreshTokenModel.findOneAndUpdate(
+      { tokenId: decoded.jti, revoked: false },
+      { revoked: true },
+    );
+  }
+
   clearCookie(res, "refreshToken");
   apiResponse(res, 200, {
     data: null,
